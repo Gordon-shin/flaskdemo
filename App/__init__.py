261a64ee252027620163581d31829e5e3539469c
Gordon-Shin <shenchen1250@hotmail.com>
2019-11-15 21:55:43 +0800

    flask 分离mtv模型

--- App/__init__.py
+++ App/__init__.py
@@ -1,13 +1,20 @@
 from flask import Flask
+
+from App.ext import init_ext
+
 ''' 包内初始化app，加载路由'''
 
 #from App.views import init_route
 #from App.views import blue
 from App.views import init_view
-
+from flask_sqlalchemy import SQLAlchemy
 
 def create_app():
     app = Flask(__name__)
-    #init_route(app)
+    #url全格式  数据库+驱动：//用户名：密码@主机：端口/具体哪个库
+    app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///sqlite.db"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    init_ext(app)
     init_view(app)
     return app
--- App/views/firstblue.py
+++ App/views/firstblue.py
@@ -1,5 +1,32 @@
 from flask import Blueprint
+
+from App.models import *
+'''模型（bean）导入'''
+
 blue = Blueprint('blue',__name__)
 @blue.route('/',)
 def index():
     return '我是蓝图的主页'
+
+@blue.route('/h')
+def h1():
+    return '<h1>hi<h1>'
+
+@blue.route('/createdb/')
+def create_db():
+    db.create_all()
+    return  '创建成功'
+
+@blue.route('/adduser')
+def add_user():
+    user = User()
+    user.username = 'tom'
+
+
+    db.session.add(user)
+    db.session.commit()
+    return '添加成功'
+
+@blue.route('/update')
+def update_database():
+   pass
